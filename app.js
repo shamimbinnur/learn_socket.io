eb21ad0df684163f7c2f78b6f1d02a87aa9378d3
shamim <sbnsbn00@gmail.com>
2021-04-05 18:34:16 +0600

    done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io');
+
+//PORT
 const PORT = 5000;
 
 // Static files
@@ -8,5 +11,22 @@ app.use(express.static('public'))
 
 const server = app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
+})
+
+//socket setup
+const io = socket(server);
+
+// listening client connection
+io.on('connection', (socket)=>{
+    console.log("Made socket connection", socket.id);
+
+    socket.on('chat', (data)=>{
+        io.sockets.emit('chat', data)
+
+    })
+
+    socket.on('typing', (data)=>{
+        socket.broadcast.emit('typing', data)
+    })
 })
 
